871d5b15c69334a3bb5ba96363a59c0bc072efce
Laurens Valk <laurensvalk@gmail.com>
2018-03-15 21:18:04 +0100

    PEP (breaking)

--- systemsim/core.py
+++ systemsim/core.py
@@ -1,4 +1,5 @@
-"""Provides general system class from which all other systems are derived"""
+"""Provides general system class from which all other systems are derived."""
+
 
 # Use numpy for matrices and vectors
 import numpy as np
@@ -6,11 +7,13 @@ import numpy as np
 # Use odeint for integrating equations of motion
 from scipy.integrate import odeint
 
-class System:
-    """Generic system with states, inputs, outputs and simulation methods
 
-    This class defines the basic system structure from which all the other systems are derived.
-    The Equations of motion are unspecified in this base class.
+class System():
+    """Generic system with states, inputs, outputs and simulation methods.
+
+    This class defines the basic system structure from which all the other
+    systems are derived. The Equations of motion are unspecified in this
+    base class.
     """
 
     # Matrix/Vector index constants
@@ -19,8 +22,7 @@ class System:
 
     def __init__(self, n_states, n_inputs, n_outputs,
                  x_initial=None, exogenous_input_function=None):
-        """Initialize system"""
-
+        """Initialize system."""
         # Set attributes
         self.n_states = n_states
         self.n_inputs = n_inputs
@@ -30,7 +32,7 @@ class System:
         self.zero_state = np.zeros(n_states)
         self.zero_output = np.zeros(n_outputs)
         self.zero_input = np.zeros(n_inputs)
-        self.x_initial = x_initial if x_initial is not None else self.zero_state
+        self.x_initial = self.zero_state if x_initial is None else x_initial
         assert len(self.x_initial) == n_states
 
         # Empty simulation results
@@ -41,9 +43,14 @@ class System:
         # Maximum state norm that we consider before cancelling integration
         self.max_state = 1e9
 
-        # Store exogeneous generator if supplied, otherwise create zero generator
+        # Store exogeneous generator if supplied,
+        # otherwise create zero generator.
         zero_generator = lambda time: self.zero_input
-        self.exogenous_input = exogenous_input_function if exogenous_input_function is not None else zero_generator
+
+        if exogenous_input_function is None:
+            self.exogenous_input = zero_generator
+        else:
+            self.exogenous_input = exogenous_input_function
 
         # Store default signal names that may be overwritten by other classes
         self.state_names = ['x_' + str(i) for i in range(self.n_states)]
@@ -52,73 +59,85 @@ class System:
 
     @staticmethod
     def concatenate(list_of_vectors):
-        "Concatenate vectors into large vector or return empty vector if list is empty"
-        return np.concatenate(list_of_vectors) if list_of_vectors else np.empty(0)
-
+        """Concatenate vectors as large vector. Give empty vector if empty."""
+        if list_of_vectors:
+            return np.concatenate(list_of_vectors)
+        else:
+            return np.empty(0)
 
     def print_dimensions(self):
-        """Print the system size"""
+        """Print the system size."""
         print(self.__dict__)
 
     def output(self, state, time):
-        "Return output given the current state. System object returns zero by default."
+        """Return output given the current state. Returns zero by default."""
         return self.zero_output
 
     def state_feedback(self, state, time):
-        """Internal state feedback generator. Zero by default"""
+        """Generate internal state feedback. Zero by default."""
         return self.zero_input
 
     def equations_of_motion(self, state, input, time):
-        """Evaluate equations of motion"""
+        """Evaluate equations of motion."""
         return self.zero_state
 
     def state_change(self, state, external_control_input, time):
-        """Evaluate equations of motion subject to exogenous input at current time"""
+        """Evaluate equations of motion subject to exogenous input."""
         exogenous_input = self.exogenous_input(time)
         state_feedback = self.state_feedback(state, time)
         total_input = external_control_input + exogenous_input + state_feedback
         return self.equations_of_motion(state, total_input, time)
 
     def __state_change_ode(self, state, time):
-        """Ordinary differential equation for ODE solver"""
+        """Evaluate ordinary differential equation for ODE solver."""
         # Break the loop if we're exploding
-        assert np.amax(np.abs(state)) <= self.max_state, "State is too large. Unstable?"
+        assert np.amax(np.abs(state)) <= self.max_state, \
+            "State is too large. Unstable?"
         # When simulating this system on its own, the external input is zero
-        external_control_input = self.zero_input 
+        external_control_input = self.zero_input
         # Compute the equation of motion given input at current time
         return self.state_change(state, external_control_input, time)
 
-    def Simulate(self, time_range):
-        """Integrate the ordinary differential equations and store the results"""
-        self.state_trajectory = odeint(self.__state_change_ode, self.x_initial, time_range).T
-        self.simulation_time = time_range 
-        self.ComputeOutputTrajectory()
+    def simulate(self, time_range):
+        """Integrate the ordinary differential equations."""
+        self.state_trajectory = odeint(self.__state_change_ode,
+                                       self.x_initial, time_range).T
+        self.simulation_time = time_range
+        self.compute_output_trajectory()
         # Further processing if needed
-        self.PostSimulationProcessing()
-        
-    def ComputeOutputTrajectory(self):
-        # Shortcut to calculate output at a given time sample k in the simulation history
-        output = lambda k: self.output(self.state_trajectory[:, k], self.simulation_time[k]).reshape((self.n_outputs,1))
+        self.post_simulation_processing()
+
+    def compute_output_trajectory(self):
+        """Compute the outputs for the previously run simulation."""
+        # Shortcut to calculate output at a given time sample k
+        # in the simulation history
+        output = lambda k: \
+            self.output(self.state_trajectory[:, k],
+                        self.simulation_time[k]).reshape((self.n_outputs, 1))
 
         # Calculate the outputs at all time steps and concatenate results
-        self.output_trajectory = np.hstack([output(k) for (k, time) in enumerate(self.simulation_time)])
+        self.output_trajectory = np.hstack(
+            [output(k) for (k, time) in enumerate(self.simulation_time)]
+        )
 
-    def PostSimulationProcessing(self):
+    def post_simulation_processing(self):
         """Process simulation results. The basic class does nothing here."""
         pass
 
     def plot_state_trajectory(self, states='all'):
+        """Return state data dictionary compatible with Plotly Plots."""
         if states == 'all':
             states_to_plot = range(self.n_states)
         else:
-            states_to_plot = states                
-        return [{'x' : self.simulation_time,
-                 'y' : self.state_trajectory[i,:],
+            states_to_plot = states
+        return [{'x': self.simulation_time,
+                 'y': self.state_trajectory[i, :],
                  'name': self.state_names[i]
-                 }  for i in states_to_plot]
+                 } for i in states_to_plot]
 
     def plot_output_trajectory(self):
-        return [{'x' : self.simulation_time,
-                 'y' : self.output_trajectory[i,:],
+        """Return output data dictionary compatible with Plotly Plots."""
+        return [{'x': self.simulation_time,
+                 'y': self.output_trajectory[i, :],
                  'name': self.output_names[i]
-                 }  for i in range(self.n_outputs)]
+                 } for i in range(self.n_outputs)]
--- systemsim/mechanical.py
+++ systemsim/mechanical.py
@@ -1,58 +1,75 @@
+"""Provides classes for mechanical systems."""
 import numpy as np
 from .core import System
 
-class SimpleMechanicalSystem(System):
-    """Mechanical System"""
 
-    def __init__(self, M, F, C=None, G=None, Q=None,
-                 q_initial=None, q_dot_initial=None,
-                 exogenous_input_function=None):
-        """Initialize"""
+class SimpleMechanicalSystem(System):
+    """Mechanical System."""
+
+    def __init__(
+            self,
+            M,
+            F,
+            C=None,
+            G=None,
+            Q=None,
+            q_initial=None,
+            q_dot_initial=None,
+            exogenous_input_function=None):
+        """Initialize the mechanical system."""
         # Store matrices
         self.M, self.F = M, F
 
-        # Determine number of coordinates from shape of M in order to avoid the user having to supply it
+        # Determine number of coordinates from shape of M in order to avoid
+        # the user having to supply it
         for attempts in range(1, 100):
             try:
                 # Try large number of coordinates until success
                 self.n = M(np.zeros((attempts, 1))).shape[0]
                 break
-            except (IndexError, TypeError) as errors:
+            except (IndexError, TypeError):
                 pass
 
         # Initial states values and size
-        q_initial = q_initial if q_initial is not None else np.zeros(self.n)
-        q_dot_initial = q_dot_initial if q_dot_initial is not None else np.zeros(self.n)
+        if q_initial is None:
+            q_initial = np.zeros(self.n)
+        if q_dot_initial is None:
+            q_dot_initial = np.zeros(self.n)
         x_initial = np.append(q_initial, q_dot_initial)
         n_states = self.n*2
 
         # Determine input/output size by checking size of G:
         self.m = self.F(q_initial).shape[self.COL]
-        n_outputs = n_inputs = self.m                
+        n_outputs = n_inputs = self.m
 
-        # Store remaining system matrix generators or create them as zeros otherwise
-        self.C = C if C is not None else lambda q, q_dot: np.zeros((self.n,self.n))
-        self.Q = Q if Q is not None else lambda q, q_dot: np.zeros((self.n,1))
-        self.G = G if G is not None else lambda q: np.zeros((self.n,1))
+        # Store remaining system matrix generators or create
+        # them as zeros otherwise
+        self.C = C if C is not None else lambda q, q_dot: np.zeros((self.n,
+                                                                    self.n))
+        self.Q = Q if Q is not None else lambda q, q_dot: np.zeros((self.n, 1))
+        self.G = G if G is not None else lambda q: np.zeros((self.n, 1))
 
         # Initialize system object
-        System.__init__(self, n_states, n_inputs, n_outputs, x_initial, exogenous_input_function)
+        System.__init__(self, n_states, n_inputs, n_outputs,
+                        x_initial, exogenous_input_function)
 
         # Overwrite state names
-        self.state_names = ['q_' + str(i) for i in range(self.n)] + ['q_dot' + str(i) for i in range(self.n)]
+        self.state_names = ['q_' + str(i) for i in range(self.n)] + \
+                           ['q_dot' + str(i) for i in range(self.n)]
 
     def get_coordinates(self, state):
+        """Return q, q_dot from x."""
         q = state[0:self.n]
         q_dot = state[self.n:2*self.n+1]
         return q, q_dot
 
     def output(self, state, time=None):
-        "Mechanical System Output"
+        """Return mechanical system Output."""
         q, q_dot = self.get_coordinates(state)
         return self.F(q).T@q_dot
 
     def state_feedback(self, state, time=None):
-        """Internal mechanical system feedback (none for now)"""
+        """Return internal mechanical system feedback."""
         return self.zero_input
 
     def equations_of_motion(self, state, tau, time=None):
@@ -63,12 +80,7 @@ class SimpleMechanicalSystem(System):
         C = self.C(q, q_dot)
         Q = self.Q(q, q_dot)
         G = self.G(q)
-        q_dotdot = np.linalg.solve(M, Q+F@tau.reshape(self.m,1)-G-C@q_dot.reshape(self.n,1))        
+        q_dotdot = np.linalg.solve(
+            M, Q+F@tau.reshape(self.m, 1)-G-C@q_dot.reshape(self.n, 1)
+        )
         return np.append(q_dot, q_dotdot).reshape(2*self.n)
-
-# TODO DERIVE A Lagrangian and Hamiltonian variant or add way to access momenta, storage, supply etc
-# Either from above, or derived from Hamiltonian sys...
-
-
-
-
