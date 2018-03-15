1cf6ab0f778c4e8080a53d08b05ac569e7d05825
Laurens Valk <laurensvalk@gmail.com>
2018-03-15 20:14:47 +0100

    Packaging

---
+++ README.rst
@@ -0,0 +1,6 @@
+systemsim
+=========
+
+A work-in-progress Python simulator for feedback systems and networks.
+
+In development for my ongoing MSc thesis research.
---
+++ setup.py
@@ -0,0 +1,38 @@
+"""pypi compatible setup module.
+
+This setup is based on:
+https://packaging.python.org/tutorials/distributing-packages/
+
+
+"""
+from setuptools import setup
+from codecs import open
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='systemsim',
+    version='0.1',
+    description='Simulator for dynamical feedback systems and networks.',
+    long_description=long_description,
+    url='https://github.com/laurensvalk/systemsim',
+    author='Laurens Valk',
+    author_email='laurensvalk@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='dynamics simulation control',
+    python_requires='>=3',
+    packages=['systemsim'],
+    project_urls={
+        'Bug Reports': 'https://github.com/laurensvalk/systemsim/issues',
+        'Questions': 'https://github.com/laurensvalk/systemsim/issues',
+        'Source': 'https://github.com/laurensvalk/systemsim',
+    }
+    )
