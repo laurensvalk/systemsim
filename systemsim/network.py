8c2dd58196b1504eba18fbd6c85bb748fe391499
Laurens Valk <laurensvalk@gmail.com>
2018-03-16 09:49:26 +0100

    More PEP

--- systemsim/network.py
+++ systemsim/network.py
@@ -1,32 +1,42 @@
+"""Classes to create new systems out of two or more subsystems."""
+
 import numpy as np
 from .core import System
-import copy
+from copy import deepcopy
+
 
 class Interconnection(System):
-    """Multiple systems connected through inputs and outputs"""
-    def __init__(self, systems, system_connections,
-                 input_connections=None,
-                 exogenous_input_functions=None,
-                 exogenous_output_functions=None):
-        """Initialize network"""
+    """Multiple systems connected through inputs and outputs."""
+
+    def __init__(
+            self,
+            systems,
+            system_connections,
+            input_connections=None,
+            exogenous_input_functions=None,
+            exogenous_output_functions=None):
+        """Initialize network."""
         # Store systems with names
         self.systems = systems
-        # Extract names for easy reference, always in the same order when using this list
+        # Extract names for easy reference, always in the same order
+        # when using this list
         self.system_names = list(systems.keys())
 
         # Name error checking
-        assert len(self.system_names) == len(set(self.system_names)), "System names must be unique."
+        assert len(self.system_names) == len(set(self.system_names)), \
+            "System names must be unique."
 
         # Store interconnection information
         self.weights = system_connections
         # Extract list of edge tuples
         self.edges = list(system_connections.keys())
         # Create neighbor set for each system, given the edges
-        self.neighbor_names = {i: [j for j in self.system_names if (j,i) in self.edges]
-                                        for i in self.system_names}
+        self.neighbor_names = {
+            i: [j for j in self.system_names if (j, i) in self.edges] for i in self.system_names
+        }
 
         # Initial state of the whole network
-        x_initial = self.__as_state_vector({i: s.x_initial for (i, s) in self.systems.items()})        
+        x_initial = self.__as_state_vector({i: s.x_initial for (i, s) in self.systems.items()})
         n_states = sum([s.n_states for (i, s) in self.systems.items()])
         assert n_states == x_initial.shape[self.ROW]
 
@@ -56,7 +66,7 @@ class Interconnection(System):
 
         # The resulting vector of external signals acting on the network
         exogenous_input_function = lambda time: \
-            self.concatenate([self.exogenous_input_functions[signal](time) 
+            self.concatenate([self.exogenous_input_functions[signal](time)
                               for signal in self.exogenous_input_signal_names])
 
         # TODO remove this temporary depencency; just for getting output size..abs
@@ -72,53 +82,56 @@ class Interconnection(System):
         # Initialize System object
         System.__init__(self, n_states, n_inputs, n_outputs, x_initial, exogenous_input_function)
 
-
-
         # TODO: THIS STILL DOES NOT CORRECTLY TAKE exo in into account.
 
     def __as_state_per_system(self, network_state_vector):
-        """Converts a stacked vector of states back into a dictionary of states per system"""
+        """Convert a stacked vector of states back into a dictionary of states per system."""
         # As intermediate step, cut state vector into list of state vectors
         state_list = np.split(network_state_vector, self.x_cut_index)
         # Return the dictionary of states
         return {system_name: state_list[listindex] for listindex, system_name in enumerate(self.system_names)}
 
     def __as_state_vector(self, state):
-        """Converts a dictionary with states into a large stacked vector"""
+        """Convert a dictionary with states into a large stacked vector."""
         return self.concatenate([state[i] for i in self.system_names])
-    
+
     def __as_input_per_signal(self, input_vector):
-        """Converts a stacked vector of inputs back into a dictionary of input per signal"""
+        """Convert a stacked vector of inputs back into a dictionary of input per signal."""
         # As intermediate step, cut input vector into list of inputs
         input_list = np.split(input_vector, self.u_cut_index)
         # Return the dictionary of inputs
-        return {signal: input_list[listindex] for listindex, signal in enumerate(self.exogenous_input_signal_names)}    
+        return {signal: input_list[listindex] for listindex, signal in enumerate(self.exogenous_input_signal_names)}
 
     def __output_per_system(self, x, time):
-        """Return the outputs of each system, given their states"""
+        """Return the outputs of each system, given their states."""
         return {i: s.output(x[i], time) for (i, s) in self.systems.items()}
 
     def output(self, state_vector, time):
-        """Vector output when viewing this whole network as one big system"""
+        """Vector output when viewing this whole network as one big system."""
         state_per_system = self.__as_state_per_system(state_vector)
         exogenous_input_vector = self.exogenous_input(time)
         input_per_signal = self.__as_input_per_signal(exogenous_input_vector)
         output_per_system = self.__output_per_system(state_per_system, time)
-        exogenous_output_per_signal = {signal: function(output_per_system, input_per_signal) 
+        exogenous_output_per_signal = {signal: function(output_per_system, input_per_signal)
                                        for (signal, function) in self.exogenous_output_functions.items()}
         return self.concatenate([exogenous_output_per_signal[signal_name] for signal_name in self.exogenous_output_names])
 
     def distributed_law(self, x, y, time):
-        """Return the distributed control input for each system, given their outputs computed from their states
-        
-        This is the default network interaction; other interactions may be specified by overriding this one
-        
-        """        
-        return {i : sum([self.weights[(j,i)]*y[j] for j in self.neighbor_names[i] ])
-                    for (i, s) in self.systems.items()}
+        """Return the distributed control input for each system.
+
+        This is the default network interaction; other interactions may be
+        specified by overriding this one.
+        """
+        return {
+            i: sum([self.weights[(j, i)]*y[j] for j in self.neighbor_names[i]]) for (i, s) in self.systems.items()
+        }
 
     def equations_of_motion(self, state_vector, external_input_vector, time):
-        """Evaluate equations of motion including internal state feedback and subject to exogenous input at current time"""
+        """Evaluate equations of motion.
+
+        Including internal state feedback and subject to exogenous input at
+        current time.
+        """
         # Convert network state array into states per system
         state_per_system = self.__as_state_per_system(state_vector)
         # Output for each system, given current state
@@ -134,21 +147,22 @@ class Interconnection(System):
         for signal_name, systems_acted_on in self.input_connections.items():
             for system_acted_on in systems_acted_on:
                 total_input_per_system[system_acted_on] = \
-                    total_input_per_system[system_acted_on] +  external_input_per_signal[signal_name]
+                    total_input_per_system[system_acted_on] + external_input_per_signal[signal_name]
 
-        # Obtain state change for the whole network, by taking the state change for each systen
-        state_change_per_system = {i: s.state_change(state_per_system[i], total_input_per_system[i], time)
-                                    for (i, s) in self.systems.items()}
+        # Obtain state change for the whole network, by taking the state
+        # change for each systen
+        state_change_per_system = {
+            i: s.state_change(state_per_system[i], total_input_per_system[i], time) for (i, s) in self.systems.items()
+        }
 
         # Return state change in vector form
         return self.__as_state_vector(state_change_per_system)
 
     @staticmethod
     def undirected_edges(weighted_edges):
-        """For every edge in the provided network, add an according reverse edge to make the network undirected"""
-
+        """Turn every edge into an undirected one."""
         # Initialize new network topology as supplied
-        undirected_weighted_edges = copy.deepcopy(weighted_edges)
+        undirected_weighted_edges = deepcopy(weighted_edges)
 
         # For each edge in the supplied network, add complementary edge
         for (tail, head), weight in weighted_edges.items():
@@ -161,23 +175,27 @@ class Interconnection(System):
         return undirected_weighted_edges
 
     def post_simulation_processing(self):
-        """Store network simulation for all subsystems"""
+        """Store network simulation for all subsystems."""
         state_trajectory_per_system = self.__as_state_per_system(self.state_trajectory)
         for (i, s) in self.systems.items():
             s.state_trajectory = state_trajectory_per_system[i]
             s.simulation_time = self.simulation_time
             s.compute_output_trajectory()
 
+
 class DistributedSystem(Interconnection):
-    """Interconnection of systems with a common type of interaction between neighboring agents"""
+    """Interconnection of systems with a common type of interaction."""
 
     def distributed_law(self, x, y, time):
-        """ By default we evaluate the same control law for each agent, which may each have different neighbors"""        
-        return {i : self.neighbor_interaction(i, x, y, time) for (i, s) in self.systems.items()}
+        """Evaluate the same control law for each agent."""
+        return {
+            i: self.neighbor_interaction(i, x, y, time) for (i, s) in self.systems.items()
+        }
 
     def neighbor_interaction(self, me, x, y, time):
-        """Additional control input for the current agent, given the states and output of its neighbors
-        
-           By default, we use the weighted sum of the difference between the neighbor output and the output of the agent
+        """Additional control input for the current agent, given its neighbors.
+
+        By default, we use the weighted sum of the difference between the
+        neighbor output and the output of the agent.
         """
-        return sum([self.weights[(j,me)]*(y[j]-y[me]) for j in self.neighbor_names[me]])
+        return sum([self.weights[(j, me)]*(y[j]-y[me]) for j in self.neighbor_names[me]])
--- systemsim/symbolic.py
+++ systemsim/symbolic.py
@@ -1,3 +1,4 @@
+"""Tools for symbolic derivation of equations of motion."""
 from .mechanical import SimpleMechanicalSystem
 
 import sympy as sym
@@ -5,49 +6,66 @@ import cloudpickle
 
 
 def save_lambda(filename, data):
-    """Write a Python object (such as a lambda function) to a file"""
+    """Write a Python object (such as a lambda function) to a file."""
     # Save data to disk
     with open(filename, 'wb') as file_descriptor:
         file_descriptor.write(cloudpickle.dumps(data))
 
 
 def load_lambda(filename):
-    """Load a Python object (such as a lambda function) from a file"""    
+    """Load a Python object (such as a lambda function) from a file."""
     # Load a dictionary of lambda equations
-    with open(filename, 'rb') as f:    
+    with open(filename, 'rb') as f:
         return cloudpickle.load(f)
 
 
 def make_C(M, q, q_dot):
-    """Derive Coriolis matrix from mass matrix and generalized velocities"""
+    """Derive Coriolis matrix from mass matrix and generalized velocities."""
     # Dimension of the mechanical system
     n = M.shape[0]
 
-    # Standard way of deriving C matrix from the mass matrix M (See e.g. van der Schaft 2000)
-    cijk = [[[ (sym.diff(M[k,j], q[i]) + sym.diff(M[k,i], q[j]) - sym.diff(M[i,j], q[k]))/2
-                for k in range(n)] for j in range(n)] for i in range(n)]
+    # Standard way of deriving C matrix elements from the mass matrix M
+    # (See e.g. van der Schaft 2000)
+    cijk = [
+        [
+            [
+                (sym.diff(M[k, j], q[i]) + sym.diff(M[k, i], q[j]) - sym.diff(M[i, j], q[k]))/2
+                for k in range(n)
+            ] for j in range(n)
+        ] for i in range(n)
+    ]
 
-    # Derive the elements of the C matrix                
-    ckj = [[sum([cijk[i][j][k]*q_dot[i] for i in range(n)])
-                        for j in range(n)] for k in range(n)]
+    # Derive the elements of the C matrix
+    ckj = [
+        [
+            sum([cijk[i][j][k]*q_dot[i] for i in range(n)]) for j in range(n)
+        ] for k in range(n)
+    ]
 
     # Return the nested list as a matrix
     return sym.Matrix(ckj)
 
 
 def make_G(V, q):
-    """Derive generalized gravity vector from potential energy expression"""
+    """Derive generalized gravity vector from potential energy expression."""
     # Shape to 1x1 matrix (scalar)
     V = sym.Matrix([V])
-    
+
     # Return jacobian as a column vector
     return V.jacobian(q).T
 
 
 class SymbolicMechanicalSystem(SimpleMechanicalSystem):
-    def __init__(self, modelfile, parameters, q_initial=None, q_dot_initial=None, exogenous_input_function=None):    
-        """Load previously derived model equations from a file"""
+    """Mechanical system with equations of motion derived symbolically."""
 
+    def __init__(
+            self,
+            modelfile,
+            parameters,
+            q_initial=None,
+            q_dot_initial=None,
+            exogenous_input_function=None):
+        """Load previously derived model equations from a file."""
         # Load a dictionary of lambda equations from a file
         model = load_lambda(modelfile)
 
@@ -59,16 +77,20 @@ class SymbolicMechanicalSystem(SimpleMechanicalSystem):
         # Create corresponding ordered list of their values
         self.parameter_values = [parameters[name] for name in self.parameter_names]
 
-        # Create the lambda functions, with the parameter values already substituted
+        # Create the lambda functions, with the parameter values already
+        # substituted
         M = lambda q: model['M'](q, *self.parameter_values)
         G = lambda q: model['G'](q, *self.parameter_values)
         F = lambda q: model['F'](q, *self.parameter_values)
         C = lambda q, q_dot: model['C'](q, q_dot, *self.parameter_values)
         Q = lambda q, q_dot: model['Q'](q, q_dot, *self.parameter_values)
-        SimpleMechanicalSystem.__init__(self, M, F, C, G, Q, q_initial, q_dot_initial, exogenous_input_function)
+        SimpleMechanicalSystem.__init__(self, M, F, C, G, Q,
+                                        q_initial, q_dot_initial,
+                                        exogenous_input_function)
 
         # If the model already contained a state feedback law, apply it
         if 'tau' in model:
-            self.state_feedback = lambda x, time: model['tau'](self.get_coordinates(x)[0], # q
-                                                               self.get_coordinates(x)[1], # q_dot
-                                                               *self.parameter_values)
+            self.state_feedback = lambda x, time: model['tau'](
+                self.get_coordinates(x)[0],  # q
+                self.get_coordinates(x)[1],  # q_dot
+                *self.parameter_values)
